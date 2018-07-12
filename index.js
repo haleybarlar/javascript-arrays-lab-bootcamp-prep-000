f67a4e48d4b489d63c6d9d890e7d2ac21aa397b5
Haley Someone <hbarlar1@gmail.com>
2018-07-12 16:37:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newArray = ["George", ...kittens];
+  var newArray = ["Broom", ...kittens];
   newArray.push(name);
   return newArray
 }
